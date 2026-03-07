7f8ba8bd4b01d8bae2741e07a4f10f34921d2aa0
ikromr04 <ikromr04@gmail.com>
2026-03-08 02:36:12 +0500

    chore(database): add RoleSeeder with default roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            ['name' => 'admin', 'display_name' => 'Администратор'],
+            ['name' => 'manager', 'display_name' => 'Менеджер'],
+            ['name' => 'employee', 'display_name' => 'Сотрудник'],
+        ];
+
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
+    }
+}
