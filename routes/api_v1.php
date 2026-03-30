d309e3f5f8f252d757fdb8e0b7e00e89cdd50c25
ikromr04 <ikromr04@gmail.com>
2026-03-30 14:14:19 +0500

    fix(auth): change logout API method from POST to DELETE

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -8,7 +8,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me'])->name('me');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::delete('/tokens', [AuthController::class, 'logoutAll'])->name('logoutAll');
 
     Route::get('/user', function (Request $request) {
