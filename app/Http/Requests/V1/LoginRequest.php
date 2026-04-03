92105adaffba396c2be50dc8abca81a452c4f201
ikromr04 <ikromr04@gmail.com>
2026-04-03 17:28:46 +0500

    fix: use email instead of login in LoginRequest

--- app/Http/Requests/V1/LoginRequest.php
+++ app/Http/Requests/V1/LoginRequest.php
@@ -23,8 +23,8 @@ class LoginRequest extends FormRequest
     {
         return [
             'data.type' => ['required', 'in:tokens'],
-            'data.attributes.email' => ['required', 'email'],
-            'data.attributes.password' => ['required', 'string', 'min:6']
+            'data.attributes.email' => ['required', 'string', 'email', 'max:150'],
+            'data.attributes.password' => ['required', 'string', 'min:6', 'max:50'],
         ];
     }
 
@@ -32,16 +32,25 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => $this->input('data.attributes.email'),
-            'password' => $this->input('data.attributes.password')
+            'password' => $this->input('data.attributes.password'),
         ];
     }
 
-    public function attributes(): array
+    public function messages(): array
     {
         return [
-            'data.type' => 'Тип ресурса',
-            'data.attributes.email' => 'Email',
-            'data.attributes.password' => 'Пароль'
+            'data.type.required' => 'Тип ресурса обязателен.',
+            'data.type.in' => 'Неверный тип ресурса.',
+
+            'data.attributes.email.required' => 'Email обязателен.',
+            'data.attributes.email.string' => 'Email должен быть строкой.',
+            'data.attributes.email.email' => 'Некорректный формат email.',
+            'data.attributes.email.max' => 'Email не должен превышать 150 символов.',
+
+            'data.attributes.password.required' => 'Пароль обязателен.',
+            'data.attributes.password.string' => 'Пароль должен быть строкой.',
+            'data.attributes.password.min' => 'Пароль должен быть не менее 6 символов.',
+            'data.attributes.password.max' => 'Пароль не должно превышать 50 символов.',
         ];
     }
 }
