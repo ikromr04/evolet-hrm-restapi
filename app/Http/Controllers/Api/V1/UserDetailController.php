7bfb220c1d8e5a76cea8e66abb4b79cd0b8c0c9a
ikromr04 <ikromr04@gmail.com>
2026-04-08 16:12:55 +0500

    fix(user-details): fix Controller imports

--- app/Http/Controllers/Api/V1/UserDetailController.php
+++ app/Http/Controllers/Api/V1/UserDetailController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\ApiV1;
+namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\V1\UserDetailStoreRequest;
 use App\Http\Resources\V1\UserDetailResource;
